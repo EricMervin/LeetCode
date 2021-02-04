eba951334df3d6e9f1a535b8a48290aac236ea5e
Eric Mervin <ericmervin007@gmail.com>
2021-02-04 20:09:25 +0530

    Max Consecutive Ones

--- LeetCode/maxConsecutiveOnes.cpp
+++ LeetCode/maxConsecutiveOnes.cpp
@@ -1,10 +1,7 @@
 //  LeetCode - maxConsecutiveOnes.cpp
-//
 //  Created by Eric Mervin on 04/02/21.
-//
 
 #include <iostream>
-#include <chrono>
 using namespace std;
 
 int maxOnes(int arr[], int size){
@@ -29,20 +26,24 @@ int maxOnes(int arr[], int size){
 }
 
 int main(){
+    //Start clock
     auto start = chrono::steady_clock::now();
     
+    //Declaring variables
     int arrSize, i;
     cout << "Array Size: ";
     cin >> arrSize;
     
+    //Array initialization and declaration
     int intArr[arrSize];
-    
     for(i = 0; i < arrSize; i++){
         cin >> intArr[i];
     }
     
+    //Outputing result
     cout << "Result: " << maxOnes(intArr, arrSize) << endl;
     
+    //Printing runtime
     auto end = chrono::steady_clock::now();
     double elapsedTime = double(chrono::duration_cast<chrono::nanoseconds>(end - start).count());
     cout << "Elapsed Time(s): " << elapsedTime / 1e9 << endl;
