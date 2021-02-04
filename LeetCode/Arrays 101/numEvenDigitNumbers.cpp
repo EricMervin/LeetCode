b633dde6966a558006750f079220ecabf80b734c
Eric Mervin <ericmervin007@gmail.com>
2021-02-04 22:27:52 +0530

    Array 101 Group

---
+++ LeetCode/Arrays 101/numEvenDigitNumbers.cpp
@@ -0,0 +1,16 @@
+//  LeetCode - numEvenDigitNumbers.cpp
+//  Created by Eric Mervin on 04/02/21.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Start clock
+    auto start = chrono::steady_clock::now();
+    
+    //Printing runtime
+    auto end = chrono::steady_clock::now();
+    double elapsedTime = double(chrono::duration_cast<chrono::nanoseconds>(end - start).count());
+    cout << "Elapsed Time(s): " << elapsedTime / 1e9 << endl;
+    return 0;
+}
